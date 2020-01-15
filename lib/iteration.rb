a397639d7406990df5846e1a97333cf0bd199a68
Agnes Park <agnespark824@gmail.com>
2020-01-15 05:00:50 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,9 +42,14 @@ def total_even_pairs(src)
   total = 0
   row_index = 0
   while row_index < src.count do
-    if src[row_index][element_index] % 2 == 0
-        total += src[row_index][element_index]
-        element_index += 1
+    if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
+        total += src[row_index][0]
+        total += src[row_index][1]
+      elseif src[row_index][0] % 2 == 0 && src[row_index][1] % 2 != 0
+        total += src[row_index][0]
+      elseif src[row_index][0] % 2 != 0 && src[row_index][1] % 2 != 0
+        total += src[row_index][1]
+      else 
       else element_index += 1
       end 
       row_index += 1
